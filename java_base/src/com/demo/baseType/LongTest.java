9840e7efba0af23e53d7b308ad93b0ee34b993e2
lichuang <lcup@foxmail.com>
2021-07-02 11:22:18 +0800

    add demo for test Long type

---
+++ java_base/src/com/demo/baseType/LongTest.java
@@ -0,0 +1,15 @@
+package com.demo.baseType;
+
+/**
+ * @Description
+ * @Author chuang.li
+ * @Date 2021/7/1 15:42
+ */
+public class LongTest {
+    public static void main(String[] args) {
+        long a = 1;
+        long b = 1;
+        System.out.println(a==b);
+//        System.out.println(a.equals(b));
+    }
+}
